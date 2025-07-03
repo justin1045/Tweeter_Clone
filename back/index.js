d0c834a7d6dffc9bdbc4659d0a24fe67dad06076
justin1045 <cstank786@gmail.com>
2025-07-03 20:14:31 +0530

    added user routes and controller but have some issue in update password will do it

--- back/index.js
+++ back/index.js
@@ -1,11 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from "cloudinary";
 
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js"
+
 import connectMongoDB from "./DB/connectMongoDB.js";
-import cookieParser from "cookie-parser";
 
 dotenv.config();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
 const app = express();
 
 // MIDDLEWARES FOR DATA TO PARSE
@@ -16,6 +26,7 @@ app.use(cookieParser());
 
 // ROUTES
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);   
 
 // PORT
 app.listen(process.env.PORT, ()=> {
