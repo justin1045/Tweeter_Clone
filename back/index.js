c723b3270d6fbc4ba499bfa02f32ede095d4a0cc
justin1045 <cstank786@gmail.com>
2025-08-01 18:28:35 +0530

    i have updated the size of the file you can upload at the time of the post

--- back/index.js
+++ back/index.js
@@ -22,7 +22,7 @@ cloudinary.config({
 const app = express();
 
 // MIDDLEWARES FOR DATA TO PARSE
-app.use(express.json());
+app.use(express.json({limit: "4mb"}));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
