3b6584285cfe7a3e0e1ae1d8d81c2e8cf263c619
justin1045 <cstank786@gmail.com>
2025-08-05 16:50:11 +0530

    debugging

--- back/routes/notification.route.js
+++ back/routes/notification.route.js
@@ -7,6 +7,5 @@ const router = express.Router();
 
 router.get("/", protectRoute, getNotifications);
 router.delete("/", protectRoute, deleteNotifications);
-// router.delete("/:id", protectRoute, deleteNotification);
 
 export default router;
